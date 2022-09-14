e3b31de8fad52293b60cb9812e28771c150bf622
AishwariyaRanjan <ranjanaishwariya107@gmail.com>
2022-09-13 19:49:04 -0700

    Initial commit - basic structure

---
+++ app/src/main/java/com/example/wordle/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.wordle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    private fun checkGuess(guess: String) : String {
+        var result = ""
+        for (i in 0..3) {
+            if (guess[i] == wordToGuess[i]) {
+                result += "O"
+            }
+            else if (guess[i] in wordToGuess) {
+                result += "+"
+            }
+            else {
+                result += "X"
+            }
+        }
+        return result
+    }
+}
