63680455831fb31eaa15f0bbb4bbebe1dbc22781
AishwariyaRanjan <ranjanaishwariya107@gmail.com>
2022-09-16 07:39:37 -0700

    first working version

--- app/src/main/java/com/example/wordle/MainActivity.kt
+++ app/src/main/java/com/example/wordle/MainActivity.kt
@@ -12,33 +12,50 @@ import android.widget.TextView
 import androidx.core.view.isVisible
 
 import com.example.wordle.FourLetterWordList.getRandomFourLetterWord
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
     var guessCounter = 0
-//    val wordToGuess = getRandomFourLetterWord()
-    var wordToGuess = "book"
+    var wordToGuess = getRandomFourLetterWord()
     var strValue=""
     lateinit var userGuess : EditText
-    val guess1 = findViewById<TextView>(R.id.guess1)
-    val guessCheck1 = findViewById<TextView>(R.id.guessCheck1)
-    val guess2 = findViewById<TextView>(R.id.guess2)
-    val guessCheck2 = findViewById<TextView>(R.id.guessCheck2)
-    val guess3 = findViewById<TextView>(R.id.guess3)
-    val guessCheck3 = findViewById<TextView>(R.id.guessCheck3)
-    val guessButton = findViewById<Button>(R.id.guessButton)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val guess1 = findViewById<TextView>(R.id.guess1)
+        val guessCheck1 = findViewById<TextView>(R.id.guessCheck1)
+        val guess2 = findViewById<TextView>(R.id.guess2)
+        val guessCheck2 = findViewById<TextView>(R.id.guessCheck2)
+        val guess3 = findViewById<TextView>(R.id.guess3)
+        val guessCheck3 = findViewById<TextView>(R.id.guessCheck3)
+        val guessButton = findViewById<Button>(R.id.guessButton)
+        val correctWord = findViewById<TextView>(R.id.wordToGuess)
+
         guessButton.setOnClickListener{
-            if (guessButton.text=="New game"){
-//                resetAll()
+            if (guessButton.text=="New Game"){
                 Log.d("TestingReset","entered new game if statement")
+
+                //clear textviews
+                guess1.text=""
+                guessCheck1.text=""
+                guess2.text=""
+                guessCheck2.text=""
+                guess3.text=""
+                guessCheck3.text=""
+
+                //reset counter
+                guessCounter=0
+                //reset guess button
+                guessButton.text="Guess!"
+
                 //generate new word to guess
                 wordToGuess = getRandomFourLetterWord()
                 Log.d("TestingWord",wordToGuess.toString())
+                correctWord.isVisible=false
             }
             else{
                 guessCounter++
@@ -48,18 +65,17 @@ class MainActivity : AppCompatActivity() {
                 strValue = userGuess.text.toString()
 
                 var guessCheckResult=""
-                val correctWord = findViewById<TextView>(R.id.wordToGuess)
-                //displaying user guess and guess check
 
+                //displaying user guess and guess check
                 if(guessCounter==1){
                     guess1.text=strValue
-                    guessCheckResult = checkGuess(strValue)
+                    guessCheckResult = checkGuess(strValue.toUpperCase(Locale.ROOT))
                     guessCheck1.text = guessCheckResult
                     Log.d("Testing1",guessCheckResult)
                 }
                 if(guessCounter==2){
                     guess2.text=strValue
-                    guessCheckResult = checkGuess(strValue)
+                    guessCheckResult = checkGuess(strValue.toUpperCase(Locale.ROOT))
                     guessCheck2.text = guessCheckResult
                     Log.d("Testing2",guessCheckResult)
                 }
@@ -68,32 +84,23 @@ class MainActivity : AppCompatActivity() {
                     guessButton.text = "New Game"
 
                     guess3.text=strValue
-                    guessCheckResult = checkGuess(strValue)
+                    guessCheckResult = checkGuess(strValue.toUpperCase(Locale.ROOT))
                     guessCheck3.text = guessCheckResult
 
                     //make wordToGuess visible
                     correctWord.text=wordToGuess
                     correctWord.isVisible=true
                 }
-            }
+                //reser editText for user
+                userGuess.setText("")
+            }//end of else
         }
     }
-    private fun resetAll(){
-        //clear textviews
-        guess1.text=""
-        guessCheck1.text=""
-        guess2.text=""
-        guessCheck2.text=""
-        guess2.text=""
-        guessCheck2.text=""
 
-        //reset counter
-        guessCounter=0
-        //reset guess button
-        guessButton.text="Guess!"
-    }
     private fun checkGuess(guess: String) : String {
         var result = ""
+        Log.d("testingCheckGuess - guess",guess.toString())
+        Log.d("testingCheckGuess - wordToGuess",wordToGuess.toString())
         for (i in 0..3) {
             if (guess[i] == wordToGuess[i]) {
                 result += "O"
